a3d9b0c25da0547ec59daf684790fb3ad6739b04
Nico Terevinto <pabtecha@gmail.com>
2018-10-19 16:52:23 +0200

    App de héroes creada. Añadida funcionalidad de buscador. Versión básica

---
+++ 02-spa/src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {HeroesServices, Heroes} from '../../services/heroes.services';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
+})
+export class BuscadorComponent implements OnInit {
+
+  heroes: Heroes[] = [];
+  texto: string;
+  constructor(private activatedRoute: ActivatedRoute,
+              private _heroesService: HeroesServices) {
+
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(params => {
+      this.texto = params['texto'];
+      this.heroes = this._heroesService.buscarHeroes(params['texto']);
+    });
+  }
+
+}
